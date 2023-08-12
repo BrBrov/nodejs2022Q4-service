9a76cc60c24d9544a540c96a42e275e079655896
BrBrov <epoc@list.ru>
2023-08-12 23:26:54 +0300

    feat: create artists logic

--- src/controllers/artist/artist.service.ts
+++ src/controllers/artist/artist.service.ts
@@ -1,20 +1,48 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ArtistDto, Artist } from 'src/models/artist-models';
+import ArtistEntity from 'src/type-orm/entity/artist-entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export default class ArtistService {
-  // public getAllArtists(): Array<Artist> {
-  //   return this.db.getAllArtists();
-  // }
-  // public getArtist(id: string): Artist | null {
-  //   return this.db.getArtist(id);
-  // }
-  // public createArtist(dto: ArtistDto): Artist {
-  //   return this.db.createArtist(dto);
-  // }
-  // public updateArtist(id: string, dto: ArtistDto): Artist | null {
-  //   return this.db.updateArtist(id, dto);
-  // }
-  // public deleteArtist(id: string): boolean {
-  //   return this.db.deleteArtist(id);
-  // }
+  constructor(
+    @InjectRepository(ArtistEntity) private db: Repository<ArtistEntity>,
+  ) {}
+
+  public async getAllArtists(): Promise<Array<Artist>> {
+    return await this.db.find();
+  }
+
+  public async getArtist(id: string): Promise<Artist> {
+    return await this.db.findOne({
+      where: {
+        id: id,
+      },
+    });
+  }
+
+  public async createArtist(dto: ArtistDto): Promise<Artist> {
+    return await this.db.save(dto);
+  }
+
+  public updateArtist(id: string, dto: ArtistDto): Promise<Artist | null> {
+    return this.db.update({ id }, { ...dto }).then((result) => {
+      if (result.affected === 0) return null;
+
+      return this.db.findOne({
+        where: {
+          id: id,
+        },
+      });
+    });
+  }
+
+  public async deleteArtist(id: string): Promise<boolean> {
+    return await this.db.delete(id).then((result) => {
+      if (result.affected === 0) return false;
+
+      return true;
+    });
+  }
 }
